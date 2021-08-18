e1ab81af8b0c519d513ee7aed328ce016bb54cc1
mahsak01 <mahsakarimi01@yahoo.com>
2021-08-18 21:17:11 +0430

    fix bug of sign in page

--- autoGalleryApp/lib/screens/signIn.dart
+++ autoGalleryApp/lib/screens/signIn.dart
@@ -20,195 +20,177 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Theme.of(context).backgroundColor,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
+        body: Container(
+      color: Theme.of(context).backgroundColor,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (overscroll) {
+          overscroll.disallowGlow();
+        },
+        child: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
           children: [
-
             /*
               ************************** set top background of page  **************************
              */
-            SafeArea(
-              child: SizedBox(
-                height: 30.h,
-                width: 100.w,
-                child: Stack(
-                  alignment: Alignment.topLeft,
-                  children: [
-                    ClipPath(
-                      clipper: WaveClipperTwo(),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height / 3.3,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("assets/images/2.jpg"),
-                              fit: BoxFit.cover,
-                              colorFilter: new ColorFilter.mode(
-                                  Theme.of(context)
-                                      .accentColor
-                                      .withOpacity(0.5),
-                                  BlendMode.softLight)),
-                        ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 4,
+              width: 100.w,
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  ClipPath(
+                    clipper: WaveClipperTwo(),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 3.3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/2.jpg"),
+                            fit: BoxFit.cover,
+                            colorFilter: new ColorFilter.mode(
+                                Theme.of(context).accentColor.withOpacity(0.5),
+                                BlendMode.softLight)),
                       ),
                     ),
+                  ),
 
-                    /*
-                            ************************** icon for go to last page **************************
+                  /*
+                          ************************** icon for go to last page **************************
 
-                           */
-                    FadeAnimation(
-                      1,
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: IconButton(
-                            icon: Icon(
-                              Icons.arrow_back_ios,
-                              color: Theme.of(context).backgroundColor,
-                              size: 50,
-                            ),
-                            onPressed: () {
-                              Navigator.pop(context);
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SplashScreen()),
-                              );
-                            }),
-                      ),
+                         */
+                  FadeAnimation(
+                    1,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            color: Theme.of(context).backgroundColor,
+                            size: 50,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SplashScreen()),
+                            );
+                          }),
                     ),
-                  ],
+                  ),
+                ],
+              ),
+            ),
+            /*
+                  ************************** set title of page  **************************
+                 */
+            FadeAnimation(
+              2,
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: Text(
+                  "ورود",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                      color: Theme.of(context).accentColor, fontSize: 15.w),
                 ),
               ),
             ),
-            Expanded(
-              child: NotificationListener<OverscrollIndicatorNotification>(
-                onNotification: (overscroll) {
-                  overscroll.disallowGlow();
-                },
-                child: ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  children: [
+            SizedBox(
+              height: 3.h,
+            ),
+            /*
+                /*
+                    **************************    TextField of this page **************************
+                 */
+                 */
+            FadeAnimation(2.5, TextFiled(Icons.person, "ایمیل/نام کاربری")),
+            SizedBox(
+              height: 2.h,
+            ),
+            FadeAnimation(3, PasswordTextFiled()),
+            SizedBox(
+              height: 2.h,
+            ),
 
-                    /*
-                      ************************** set title of page  **************************
-                     */
-                    FadeAnimation(
-                      2,
-                      Padding(
-                        padding: const EdgeInsets.only(right: 30),
-                        child: Text(
-                          "ورود",
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                              color: Theme.of(context).accentColor, fontSize: 15.w),
-                        ),
+            /*
+                 ************************** Button of sign in to app  **************************
+                 */
+
+            FadeAnimation(3.5, Button("ورود")),
+            SizedBox(
+              height: 2.h,
+            ),
+
+            /*
+                 ************************** Text of page when don't have account **************************
+                 **************************  go to sign up page **************************
+                 */
+
+            FadeAnimation(
+              4,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
+                      );
+                    },
+                    child: Text(
+                      "ثبت نام",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Theme.of(context).accentColor,
+                        fontSize: 27,
                       ),
                     ),
-                    SizedBox(
-                      height: 3.h,
-                    ),
-                    /*
-                    /*
-                        **************************    TextField of this page **************************
-                     */
-                     */
-                    FadeAnimation(2.5, TextFiled(Icons.person, "ایمیل/نام کاربری")),
-                    SizedBox(
-                      height: 2.h,
-                    ),
-                    FadeAnimation(3, PasswordTextFiled()),
-                    SizedBox(
-                      height: 2.h,
-                    ),
-
-                    /*
-                     ************************** Button of sign in to app  **************************
-                     */
-
-                    FadeAnimation(3.5, Button("ورود")),
-                    SizedBox(
-                      height: 2.h,
-                    ),
-
-
-                    /*
-                     ************************** Text of page when don't have account **************************
-                     **************************  go to sign up page **************************
-                     */
-
-                    FadeAnimation(
-                      4,
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pop(context);
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => SignUp()),
-                              );
-                            },
-                            child: Text(
-                              "ثبت نام",
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                color: Theme.of(context).accentColor,
-                                fontSize: 27,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(18),
-                            child: Text(
-                              "حساب کاربری ندارید؟",
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 27,
-                              ),
-                            ),
-                          ),
-                        ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(18),
+                    child: Text(
+                      "حساب کاربری ندارید؟",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 27,
                       ),
                     ),
-
-
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
 
             /*
-                    ************************** set bottom background of page **************************
-                   */
-            SafeArea(
-              child: SizedBox(
-                width: 100.w,
-                height: MediaQuery.of(context).size.height / 7,
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    ClipPath(
-                      clipper: WaveClipperOne(reverse: true),
-                      child: Container(
-                        width: 100.w,
-                        height: MediaQuery.of(context).size.height / 7,
-                        color: Theme.of(context).accentColor,
-                      ),
+              ************************** set bottom background of page **************************
+               */
+            SizedBox(
+              width: 100.w,
+              height: MediaQuery.of(context).size.height / 4,
+              child: Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  ClipPath(
+                    clipper: WaveClipperOne(reverse: true),
+                    child: Container(
+                      width: 100.w,
+                      height: MediaQuery.of(context).size.height / 7,
+                      color: Theme.of(context).accentColor,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
       ),
-    );
+    ));
   }
 }
